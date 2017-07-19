e132a298fb71e2c3ac5fd6106c4850322c0f486a
Ahmet0172 <p.ahmet172@gmail.com>
2017-07-20 00:39:31 +0300

    test

--- inc/alt.php
+++ inc/alt.php
@@ -104,75 +104,12 @@ Linkedin'da firmamızı takip edebilirsiniz,
 <!-- Latest compiled and minified JavaScript -->
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script type="text/javascript">
-    /*var Resource = (function () {return {
-        Language : 'tr',
-        Urls : {
-          setQuality: '/tr/ajax_setquality/',
-getTranslatableLanguages: 'fiyat-hesap-jq.php',
-removeFile: '/tr/dosya-kaldir/',
-updateOrder: 'siparis-guncelle.php',
-applyVoucher: '/tr/kupon-uygula/',
-applyProposalVoucher: '/tr/teklif-kupon-uygula/',
-updateTransactionType: '/tr/odeme-degistir/',
-capturePaymentStatus: '/tr/ajax_capturepaymentstatus/',
-getEstimatedDate: '/tr/takribi-teslimat-tarihi/',
-updateProposal: '/tr/teklif-guncelle/',
-setCurrency: '/tr/para-birimi-sec/',
-validatePhone: '/tr/ajax_validatephone/',
-loadCatJobStatus: '/tr/cat-durum-guncelle/',
-startHighBudgetOrder: '/tr/ozel-siparis-baslat/',
-loadCatProject: '/tr/cat-proje-yukle/',
-triggerManualOrder: '/tr/ozel-siparis/'},
-Text : {FILE_WORD_COUNT: '{word_count} kelime',
-REMOVE_FILE: 'Dosyayı kaldır',
-FILE_BEING_PROCESSED: 'Dosyanızın kelime sayımı gerçekleştiriliyor. Lütfen bekleyin.',
-TEXT_BEING_PROCESSED: 'Metninizin kelime sayımı gerçekleştiriliyor. Lütfen bekleyin.',
-TOTAL_WORD_COUNT: 'Kelime Sayısı: {word_count}',
-TRANSLATION_LANGUAGES: 'Çeviri Dilleri',
-ESTIMATED_DUE_DATE: 'Tahmini bitiş',
-INTERPRETION_SERVICE: 'Hizmet Tipi',
-INTERPRETION_QUALITY: 'Uzmanlık Alanı',
-NOTARISATION_REQUIRES_CERTIFIED_TRANSLATOR: 'Noter onayı ile yalnızca Yeminli Tercüman opsiyonu seçilebilir.',
-MANUAL_ORDER_AMOUNT_CALCULATION_REQUIRED: 'Belgelerinizdeki tekrarları tespit edip size uygun fiyatlı bir teklif yollayabilmemiz için lütfen tıklayın.',
-ORDER_CONTAINS_UNCOUNTABLE_DOCUMENT: 'Kelime sayımı yapılamayan belgeleriniz var. Tercüme dillerini seçip, teklif almak için tıklayın.',
-DOCUMENTS_BEING_ANALYZED_IN_CAT_TOOL: 'Maliyeti düşürmek için belgeleriniz Protranslate CAT Tool ile analiz ediliyor.',
-DONT_WANT_TO_WAIT_FOR_QUOTE: 'Beklemek istemiyorum, bana ulaşın'},
-Validation : {REQUIRED: 'Zorunlu alan',
-Order : {ORDER_MATERIAL_ENTRY_REQUIRED: 'Devam etmek için belgelerinizi yükleyiniz.',
-TARGET_LANGUAGE_REQUIRED: 'Devam etmek için hedef dil(ler)ini seçiniz.'},
-Login : {EMAIL_REQUIRED: 'Lütfen E-posta Adresinizi Giriniz!',
-EMAIL_INVALID: 'Geçersiz E-posta Adresi!',
-PASSWORD_REQUIRED: 'Lütfen Parolanızı Giriniz!'},
-NewUser : {PHONE_NUMBER_REQUIRED: 'Lütfen Telefon Numaranızı Giriniz!',
-PHONE_NUMBER_MINLENGTH: 'Lütfen en az 10 hane girin!',
-PHONE_NUMBER_INVALID: 'Geçerli bir numara girin!',
-PASSWORD_MINLENGTH: 'Parolanız minimum 6 karakterden oluşmalıdır!',
-PASSWORD_MUST_MATCH: 'Parola Tekrarı Uyuşmuyor. Lütfen Tekrar Deneyiniz!',
-FULLNAME_REQUIRED: 'Lütfen Ad ve Soyad Bilgilerinizi Giriniz!',
-AGREEMENT_APPROVAL_REQUIRED: 'Lütfen kullanıcı sözleşmesini okuyarak onaylayınız!'},
-NewTranslator: {NATIONALITY_REQUIRED: 'Lütfen uyruğunuzu yazınız.',
-ADDRESS_REQUIRED: 'Lütfen adres bilgilerini giriniz',
-UNIVERSITY_REQUIRED: 'Lütfen Üniversite bilgilerini giriniz!',
-DEPARTMENT_REQUIRED: 'Lütfen Bir Bölüm Bilgisi Giriniz!',
-GRADUATION_DATE_REQUIRED: 'Lütfen Mezuniyet Tarihi giriniz!',
-GRADUATION_DATE_INVALID: 'Lütfen &quot;gün/ay/yıl&quot; şeklinde girin!'},
-Payment: {CARD_NO_REQUIRED: 'Kart No gerekli',
-CARD_EXPIRY_REQUIRED: 'Ay ve yıl giriniz!',
-CARD_CVV_REQUIRED: 'CVV gerekli',
-FILE_REQUIRED: 'Lütfen yüklenecek belgeyi seçiniz!'},
-Account: {ADDRESS_REQUIRED: 'Lütfen adres bilgilerini giriniz',
-CITY_REQUIRED: 'Lütfen bir şehir seçiniz',
-ID_NUMBER_REQUIRED: 'TC Kimlik No Giriniz!',
-COMPANY_NAME_REQUIRED: 'Firma Adını Giriniz!',
-TAX_OFFICE_REQUIRED: 'Vergi Dairesini Giriniz!',
-TAX_NUMBER_REQUIRED: 'Vergi Numarasını Giriniz!'}
-},
-LanguageMultiSelectOptions : {}};})();*/</script>
+
 
     <script src="js/app.min.js"></script>
+    <!--<script src="js/jquery.fileuploader.min.js"></script>-->
+    <script src="js/jquery.fileuploader.js"></script>
         
-
                 
     <script type="text/javascript">
         jQuery(document).ready(function() {
